42616a4d90a4c48a982279c713e53f5cc21f4b0c
Thiago <zazirskas@hotmail.com>
2022-05-09 20:04:45 -0300

    Initial commit with hero component

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,77 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export const Hero = () => {
+  return (
+    <Box as="section">
+      <Box
+        maxW="3xl"
+        mx="auto"
+        px={{ base: '6', lg: '8' }}
+        py={{ base: '16', sm: '20' }}
+        textAlign="center"
+      >
+        <Heading
+          my="4"
+          fontSize={{ base: '3xl', md: '5xl' }}
+          fontWeight="extrabold"
+          letterSpacing="tight"
+          lineHeight="1.2"
+        >
+          Bem-vindo ao sistema de cadastro de contatos da{' '}
+          <Box
+            as="mark"
+            bg="unset"
+            color={useColorModeValue('blue.600', 'blue.200')}
+            whiteSpace="nowrap"
+          >
+            Esferas Software
+          </Box>
+        </Heading>
+        <Text fontSize="lg" maxW="xl" mx="auto">
+          Um sistema feito para você organizar os seus contatos da melhor
+          maneira possível!
+        </Text>
+        <Stack
+          direction={{ base: 'column', sm: 'row' }}
+          mt="10"
+          justify="center"
+          spacing={{ base: '3', md: '5' }}
+          maxW="md"
+          mx="auto"
+        >
+          <Button
+            as="a"
+            href="#"
+            size="lg"
+            h="16"
+            px="10"
+            colorScheme="blue"
+            fontWeight="bold"
+            flex={{ md: '2' }}
+          >
+            Registrar um novo contato
+          </Button>
+          <Button
+            as="a"
+            flex={{ md: '1' }}
+            variant="outline"
+            href="#"
+            size="lg"
+            h="16"
+            px="10"
+            fontWeight="bold"
+          >
+            Ver contatos
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
