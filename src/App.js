55d0bf04ff78e60606c06383e2d95a42104fd48e
Thiago <zazirskas@hotmail.com>
2022-05-09 21:17:50 -0300

    Feature: Added browser router to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Hero } from './components/hero'
+import { Hero } from './components/hero';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <Hero />
-        </Grid>
-      </Box>
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Box textAlign="center" fontSize="xl">
+                  <Hero />
+                </Box>
+              }
+            />
+            <Route
+              path="/home"
+              element={<Box textAlign="center" fontSize="xl"></Box>}
+            />
+          </Routes>
+        </BrowserRouter>
+      </Grid>
     </ChakraProvider>
   );
 }
